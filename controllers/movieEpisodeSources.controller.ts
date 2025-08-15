9e8dae9fe1073a60722c5ada74e8dc1524a1f1f8
Koert Weber <koertjrweberp@gmail.com>
2025-08-15 14:29:29 +0200

    feat: added source parser with puppeteer

--- controllers/movieEpisodeSources.controller.ts
+++ controllers/movieEpisodeSources.controller.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
-import { EpisodeServerResponse } from "../types/controllers/movieEpisodeSources";
-import { SERVERS } from "../config/vidstream";
 
 import { USER_AGENT_HEADER, ACCEPT_ENCODING_HEADER, ACCEPT_HEADER, SRC_AJAX_URL } from "../config/axois";
 import axios from "axios";
-import {
-    RabbitStream
-} from "../parsers/index";
+import puppeteer from "puppeteer";
+import { EpisodeSourcesResponse } from "../types/controllers/movieEpisodeSources";
 
 // GET /movie/:id/sources?episodeId=string&serverId=string
-export default async function (req: any, res: Response) {
+export default async function (req: Request, res: Response) {
     const serverAjaxResponse = await axios.get(`${SRC_AJAX_URL}/movie/episode/server/sources/${req.query.serverId}` as string, {
         headers: {
             "Alt-Used": "vidstream.to",
@@ -21,5 +18,59 @@ export default async function (req: any, res: Response) {
         },
     });
 
-    res.send(await RabbitStream(serverAjaxResponse.data.data.link, "https://vidstream.to"));
+
+    let m3u8File: string | null = null;
+    let vttFiles: string[] = [];
+
+    const browser = await puppeteer.launch({
+        browser: 'firefox',
+        executablePath: '/usr/bin/firefox',
+    });
+
+    const page = await browser.newPage();
+    await page.setExtraHTTPHeaders({
+        Referer: `https://vidstream.to/series/${req.params.id}/${req.query.episodeId}`,
+        Origin: "https://vidstream.to",
+        "Sec-Fetch-Dest": "iframe",
+    });
+
+    page.on('request', (request) => {
+        if (request.url().endsWith('.m3u8') && !m3u8File) {
+            m3u8File = request.url();
+        } else if (request.url().endsWith('.vtt')) {
+            vttFiles.push(request.url());
+        }
+    });
+
+    await page.goto(serverAjaxResponse.data.data.link);
+    
+    let intervalId: NodeJS.Timeout | null = null;
+    await Promise.any([
+        new Promise(resolve => setTimeout(resolve, 5000)),
+        new Promise<void>(r => intervalId = setInterval(async () => {
+            if (m3u8File)
+                r();
+        }, 200)),
+    ]);
+    clearInterval(intervalId!);
+
+    await browser.close();
+
+    if (!m3u8File)
+        throw new Error("Failed to retrieve m3u8 file");
+
+    res.json({
+        sources: [
+            {
+                src: m3u8File,
+                type: "hls",
+            },
+        ],
+        tracks: vttFiles.map(file => ({
+            file,
+            label: file.split('/').pop()?.split('.')[0] || "Unknown",
+            kind: "subtitles",
+            default: false,
+        })),
+    } as EpisodeSourcesResponse);
 }
