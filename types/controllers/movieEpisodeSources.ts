0616bc4a5445fe8f43df55d79063d2b39053dc6c
Koert Weber <koertjrweberp@gmail.com>
2025-08-15 14:29:29 +0200

    feat: added source parser with puppeteer

--- types/controllers/movieEpisodeSources.ts
+++ types/controllers/movieEpisodeSources.ts
@@ -1,4 +1,13 @@
-export interface EpisodeServerResponse {
-
+export interface EpisodeSourcesResponse {
+    sources: {
+        src: string;
+        type: string;
+    }[];
+    tracks: {
+        file: string;
+        label: string;
+        kind: string;
+        default?: boolean;
+    }[];
 }
 
